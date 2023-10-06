4a515e1498b928e237404c1cefa04d40bf465b27
bahriztr <bahriztr3432@gmail.com>
2023-10-06 19:03:29 +0300

    +Fixed EnemyController with abstract class

--- Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -1,28 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Abstract.Controllers;
 
 namespace Controllers
 {
-    public class EnemyController : MonoBehaviour
+    public class EnemyController : LifeCycleController
     {
-        [Header("Timer")]
-        [SerializeField] private float maxLifeTime = 5f;
-        private float _currentTime;
-
-        void Update()
-        {
-            _currentTime += Time.deltaTime;
-
-            DestroyCondition();
-        }
-
-        private void DestroyCondition()
-        {
-            if (_currentTime >= maxLifeTime)
-            {
-                Destroy(this.gameObject);
-            }
-        }
+        
     }
 }
