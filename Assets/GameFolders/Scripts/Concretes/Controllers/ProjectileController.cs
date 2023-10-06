327082584756f9ba095c9b2a6ef7b44e00a761ff
bahriztr <bahriztr3432@gmail.com>
2023-10-06 19:01:42 +0300

    +ProjectileController script added

---
+++ Assets/GameFolders/Scripts/Concretes/Controllers/ProjectileController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Abstract.Controllers;
+
+namespace Controllers
+{
+    public class ProjectileController : LifeCycleController
+    {
+      
+    }
+}
+
