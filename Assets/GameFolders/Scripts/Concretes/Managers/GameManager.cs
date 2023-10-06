bd2e9b9db26f9eac24d638a6793d4429589b585f
bahriztr <bahriztr3432@gmail.com>
2023-10-06 15:07:18 +0300

    +GameManager script added

---
+++ Assets/GameFolders/Scripts/Concretes/Managers/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }
+
+    private void Awake()
+    {
+        SingletonThisGameObject();
+    }
+     
+    private void SingletonThisGameObject()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        else
+            Destroy(this.gameObject);
+    }
+
+    public void RestartGame()
+    {
+        StartCoroutine(RestartGameCoroutine());
+    }
+
+    private IEnumerator RestartGameCoroutine()
+    {
+        yield return SceneManager.LoadSceneAsync("Level1");
+    }
+}
